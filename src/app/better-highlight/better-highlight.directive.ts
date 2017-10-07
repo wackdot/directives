8dcfed4f36fa17baa136557dd69da27815c6db14
wackdot <e0167324@u.nus.edu>
2017-10-07 09:06:10 +0800

    6/10/17

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,27 +1,44 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+import { 
+  Directive, 
+  Renderer2, 
+  OnInit, 
+  ElementRef, 
+  HostListener, 
+  HostBinding, 
+  Input } 
+  from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
 
-  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+  // Custom property binding
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'blue';
+  
+  // Accessing the DOM properties 
+  // Access Style Property, and Sub Property Background Color  
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
   ngOnInit(){
+    this.backgroundColor = this.defaultColor;
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue'); 
   }
-
 
   // This event response applies to all elements attached with directive
   // Listens to a particular event type encapsulated within '' 
   // The event is mouseover which takes in the Event 
   @HostListener('mouseenter') mouseover(eventData: Event){
     // This is the executed when the event occurs
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue'); 
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue'); 
+    this.backgroundColor = this.defaultColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event){
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent'); 
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = this.highlightColor; 
   }
 
 
---
+++ src/app/unless.directive.ts
@@ -0,0 +1,25 @@
+import { 
+  Directive, 
+  Input, 
+  TemplateRef, 
+  ViewContainerRef } 
+  from '@angular/core';
+
+@Directive({
+  selector: '[appUnless]'
+})
+export class UnlessDirective {
+
+  // Sets the property value 
+  // Note: The function 
+  @Input() set appUnless(condition: boolean) {
+    if(!condition){
+      this.vcRef.createEmbeddedView(this.templateRef);
+    } else {
+      this.vcRef.clear();
+    }
+  }
+
+  constructor(private templateRef: TemplateRef<any>, private vcRef: ViewContainerRef) { }
+
+}
